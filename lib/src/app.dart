f0360d66bf56d96195e4a29ed7180bcc7c4d96fb
JER3MIAH <jeremiahbrownie@gmail.com>
2024-06-15 10:46:21 +0100

    Set up theme feature: - Implement logic for light/dark mode toggle

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kanban_task_manager/src/features/theme/logic/bloc/theme_state.dart';
+import 'features/theme/data/theme_data.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kanban Task Manager',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ThemeBloc()),
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (_, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Kanban Task Manager',
+            theme: state.isDarkMode ? darkTheme : lightTheme,
+          );
+        },
       ),
     );
   }
---
+++ lib/src/features/theme/data/light_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData.light(
+  useMaterial3: true,
+);

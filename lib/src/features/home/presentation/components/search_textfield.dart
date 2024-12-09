03282d2ac52f98dfc023aab073b897e97f653b07
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-09 18:17:26 +0100

    feat: Implement search text field

---
+++ lib/src/features/home/presentation/components/search_textfield.dart
@@ -0,0 +1,63 @@
+import 'package:country_stats/src/features/theme/data/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchTextfield extends StatelessWidget {
+  final TextEditingController controller;
+  const SearchTextfield({
+    super.key,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+    final tStyle = TextStyle(
+      fontSize: 13,
+      fontWeight: FontWeight.w500,
+      color: theme.onSurface,
+    );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: appColors.black.withOpacity(.06),
+            offset: Offset(0, 2),
+            blurRadius: 9,
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      child: TextField(
+        controller: controller,
+        cursorColor: theme.primary,
+        style: tStyle,
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: theme.primary,
+          border: InputBorder.none,
+          prefixIcon: Icon(
+            CupertinoIcons.search,
+            color: theme.onSurface,
+            size: 18,
+          ),
+          hintText: 'Search for a country...',
+          hintStyle: tStyle.copyWith(
+            color: theme.onSurface.withOpacity(.25),
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 5,
+            horizontal: 12,
+          ).copyWith(top: 12),
+          constraints: BoxConstraints(
+            minHeight: 56,
+            maxHeight: 56,
+            maxWidth: 480,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/features/home/presentation/screens/home_screen.dart
+++ lib/src/features/home/presentation/screens/home_screen.dart
@@ -1,20 +1,26 @@
 import 'package:country_stats/src/features/home/presentation/components/components.dart';
+import 'package:country_stats/src/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
+    final searchController = useTextEditingController();
 
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(80),
         child: CustomAppBar(),
       ),
-      body: Center(
-        child: Text('Home Screen'),
+      body: Column(
+        children: [
+          YBox(30),
+          SearchTextfield(controller: searchController),
+        ],
       ),
     );
   }

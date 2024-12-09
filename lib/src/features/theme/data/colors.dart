7a96216be630b721c8bcb0ad30e5aa9a8c314a7d
JER3MIAH <jeremiahbrownie@gmail.com>
2024-12-09 17:07:04 +0100

    style: set up theme

--- lib/src/features/theme/data/colors.dart
+++ lib/src/features/theme/data/colors.dart
@@ -5,4 +5,9 @@ var appColors = AppColors();
 class AppColors {
   final black = Color(0xFFFFFFFF);
   final white = Color(0xFF000000);
+  final darkBlue = Color(0xFF374151);
+  final darkModeBackground = Color(0xFF293241);
+  final lightModeText = Color(0xFF1E293B);
+  final lightModeInput = Color(0xFF838383);
+  final lightModeBackground = Color(0xFFFAFAFA);
 }
--- lib/src/features/theme/data/theme.dart
+++ lib/src/features/theme/data/theme.dart
@@ -1,9 +1,27 @@
+import 'package:country_stats/src/features/theme/data/colors.dart';
+import 'package:country_stats/src/shared/shared.dart';
 import 'package:flutter/material.dart';
 
 final lightTheme = ThemeData(
   useMaterial3: true,
+  scaffoldBackgroundColor: appColors.lightModeBackground,
+  textTheme: TextTheme().apply(fontFamily: PlusJakartaSans),
+  colorScheme: ColorScheme.light(
+    surface: appColors.lightModeBackground,
+    onSurface: appColors.lightModeText,
+    primary: appColors.white,
+    onPrimary: appColors.white,
+  ),
 );
 
 final darkTheme = ThemeData(
   useMaterial3: true,
+  scaffoldBackgroundColor: appColors.darkModeBackground,
+  textTheme: TextTheme().apply(fontFamily: PlusJakartaSans),
+  colorScheme: ColorScheme.dark(
+    surface: appColors.darkModeBackground,
+    onSurface: appColors.white,
+    primary: appColors.darkBlue,
+    onPrimary: appColors.white,
+  ),
 );
